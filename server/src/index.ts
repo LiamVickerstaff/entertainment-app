fef18f9dd4129fe8bcd85361a735f9cac5acd49e
Liam V <liamvickerstaff050@gmail.com>
2025-11-27 23:55:51 +1100

    Connect to redis

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,24 @@
 import express, { type Request, type Response } from "express";
 import dotenv from "dotenv";
 import authenticationRouter from "./routes/authenticationRouter";
+import { createClient } from "redis";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Redis Client
+const redisClient = createClient({
+  username: process.env.redisUsername || "",
+  password: process.env.redisPassword || "",
+  socket: {
+    host: process.env.redisHost || "",
+    port: 19561,
+  },
+});
+redisClient.on("error", (err) => console.log("Redis Client Error: ", err));
+await redisClient.connect();
 
 // Middleware
 app.use(express.json());
