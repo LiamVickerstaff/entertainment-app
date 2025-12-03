4cacbc8388acd6fff461de789441784afbf61c8e
Liam V <liamvickerstaff050@gmail.com>
2025-12-03 18:53:23 +1100

    New trending content card component

---
+++ client/src/components/ContentDisplayCards/RegularContentCard/RegularContentCard.tsx
@@ -0,0 +1,5 @@
+import styles from "./RegularContentCard.module.css";
+
+export default function RegularContentCard() {
+  return <div>RegularContentCard</div>;
+}
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,5 +1,22 @@
+import TrendingContentCard from "../../components/ContentDisplayCards/TrendingContentCard/TrendingContentCard";
 import styles from "./Home.module.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.trendingContainer}>
+        <h2>Trending</h2>
+        <div className={styles.carousel}>
+          <TrendingContentCard />
+          <TrendingContentCard />
+          <TrendingContentCard />
+          <TrendingContentCard />
+        </div>
+      </div>
+      <div className={styles.recommendedContainer}>
+        <h2>Recommended for you</h2>
+        <p>content grid</p>
+      </div>
+    </div>
+  );
 }
