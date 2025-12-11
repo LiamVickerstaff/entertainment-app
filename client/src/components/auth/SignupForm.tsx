da19e4d922135d24215109eac475a6287ceb6d27
Liam V <liamvickerstaff050@gmail.com>
2025-12-11 18:35:07 +1100

    Fixed login/signup form styling

--- client/src/components/auth/SignupForm.tsx
+++ client/src/components/auth/SignupForm.tsx
@@ -10,94 +10,100 @@ export default function SignupForm() {
     useAuth();
 
   return (
-    <div className={styles.authFormContainer}>
-      {/* Display message after non-authenticated redirect */}
-      {redirectedFromAuth && (
-        <div>
-          <p>Ready to watch all your favourite movies?</p>
-          <p>Create your account today!</p>
-        </div>
-      )}
-      <form
-        method="POST"
-        onSubmit={(e) => handleSubmit(e, "signup")}
-        noValidate
-      >
-        <h2 className={styles.formTitle}>Sign Up</h2>
-        <div className={styles.formFieldsGroup}>
-          <div
-            className={`${
-              formErrors.email && hasSubmit
-                ? styles.fieldContainerError
-                : styles.fieldContainer
-            }`}
-          >
-            <input
-              className={`${styles.emailField} ${styles.formField}`}
-              name="email"
-              type="email"
-              onChange={(e) => handleChange(e)}
-              value={formValues.email}
-              placeholder="Email Address"
-            />
-            {formErrors.email && hasSubmit && (
-              <span className={styles.errorMessage}>{formErrors.email}</span>
-            )}
+    <div className={styles.topContainer}>
+      <div className={styles.authFormContainer}>
+        {/* Display message after non-authenticated redirect */}
+        {redirectedFromAuth && (
+          <div>
+            <p>Ready to watch all your favourite movies?</p>
+            <p>Create your account today!</p>
           </div>
-          <div
-            className={`${
-              formErrors.password && hasSubmit
-                ? styles.fieldContainerError
-                : styles.fieldContainer
-            }`}
-          >
-            <input
-              className={`${styles.passwordField} ${styles.formField}`}
-              name="password"
-              type="password"
-              onChange={(e) => handleChange(e)}
-              value={formValues.password}
-              placeholder="Password"
-            />
-            {formErrors.password && hasSubmit && (
-              <span className={styles.errorMessage}>{formErrors.password}</span>
-            )}
-          </div>
-          <div
-            className={`${
-              formErrors.repeatPassword && hasSubmit
-                ? styles.fieldContainerError
-                : styles.fieldContainer
-            }`}
-          >
-            <input
-              className={`${styles.passwordField} ${styles.formField}`}
-              name="repeatPassword"
-              type="password"
-              onChange={(e) => handleChange(e)}
-              value={formValues.repeatPassword}
-              placeholder="Repeat Password"
-            />
-            {formErrors.repeatPassword && hasSubmit && (
+        )}
+        <form
+          method="POST"
+          onSubmit={(e) => handleSubmit(e, "signup")}
+          noValidate
+        >
+          <h2 className={styles.formTitle}>Sign Up</h2>
+          <div className={styles.formFieldsGroup}>
+            <div
+              className={`${
+                formErrors.email && hasSubmit
+                  ? styles.fieldContainerError
+                  : styles.fieldContainer
+              }`}
+            >
+              <input
+                className={`${styles.emailField} ${styles.formField}`}
+                name="email"
+                type="email"
+                onChange={(e) => handleChange(e)}
+                value={formValues.email}
+                placeholder="Email Address"
+              />
+              {formErrors.email && hasSubmit && (
+                <span className={styles.errorMessage}>{formErrors.email}</span>
+              )}
+            </div>
+            <div
+              className={`${
+                formErrors.password && hasSubmit
+                  ? styles.fieldContainerError
+                  : styles.fieldContainer
+              }`}
+            >
+              <input
+                className={`${styles.passwordField} ${styles.formField}`}
+                name="password"
+                type="password"
+                onChange={(e) => handleChange(e)}
+                value={formValues.password}
+                placeholder="Password"
+              />
+              {formErrors.password && hasSubmit && (
+                <span className={styles.errorMessage}>
+                  {formErrors.password}
+                </span>
+              )}
+            </div>
+            <div
+              className={`${
+                formErrors.repeatPassword && hasSubmit
+                  ? styles.fieldContainerError
+                  : styles.fieldContainer
+              }`}
+            >
+              <input
+                className={`${styles.passwordField} ${styles.formField}`}
+                name="repeatPassword"
+                type="password"
+                onChange={(e) => handleChange(e)}
+                value={formValues.repeatPassword}
+                placeholder="Repeat Password"
+              />
+              {formErrors.repeatPassword && hasSubmit && (
+                <span className={styles.errorMessage}>
+                  {formErrors.repeatPassword}
+                </span>
+              )}
+            </div>
+            {formErrors.authError && (
               <span className={styles.errorMessage}>
-                {formErrors.repeatPassword}
+                {formErrors.authError}
               </span>
             )}
           </div>
-          {formErrors.authError && (
-            <span className={styles.errorMessage}>{formErrors.authError}</span>
-          )}
-        </div>
-        <button type="submit" className={styles.submitBtn}>
-          Create an account
-        </button>
-        <p className={styles.supportMessage}>
-          Already have an account?{" "}
-          <a href="/login" className={styles.formRedirect}>
-            Login
-          </a>
-        </p>
-      </form>
+          <button type="submit" className={styles.submitBtn}>
+            Create an account
+          </button>
+          <p className={styles.supportMessage}>
+            Already have an account?{" "}
+            <a href="/login" className={styles.formRedirect}>
+              Login
+            </a>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
