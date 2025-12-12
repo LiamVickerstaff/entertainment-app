cd3631ce5502e8042a38435643b80a7bb8182480
Liam V <liamvickerstaff050@gmail.com>
2025-12-12 20:54:53 +1100

    Fix production csrf token issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,7 +37,7 @@ app.use(
           ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-CSRF-Token"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
   })
 );
 
